8ee50e33b3d0f2ac1d6600c9fc575a63830009a8
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2022-03-02 11:16:08 -0500

    Add RRE.py

--- RRE.py
+++ RRE.py
@@ -1,16 +1,16 @@
 """Puts matrices into Reduced Row Echelon Form"""
-import enum
-from functools import reduce
-from tkinter.tix import Form
 from common import *
 
 FormattedRow = list[Fraction | int]
 FormattedMatrix = list[FormattedRow]
 
 
-def _print_matrix(matrix: FormattedMatrix, format: bool=False) -> None:
-    matrix = [[(i if (not isinstance(i, Fraction) or i.denominator != 1) else i.numerator) for i in row ]for row in matrix]
+def _print_matrix(matrix: FormattedMatrix, format: bool = False) -> None:
+    matrix = [
+        [(i if (not isinstance(i, Fraction) or i.denominator != 1) else i.numerator) for i in row] for row in matrix
+    ]
     print(*matrix, sep="\n", end="\n" * 3)
+
 
 def _reduce_pos_to_zero(pos: int, r0: FormattedRow, r1: FormattedRow) -> FormattedRow:
     """Takes two rows and uses them to reduce a term at position `pos` to 0"""
@@ -20,13 +20,14 @@ def _reduce_pos_to_zero(pos: int, r0: FormattedRow, r1: FormattedRow) -> Formatt
     # Subtract from one another
     return [m - s for m, s in zip(minuend, subtrahend)]
 
+
 def divide_by_diagonal_coefficient(matrix: FormattedMatrix) -> FormattedMatrix:
     """Divides nth rows of a matrix by its coefficient at column n. Ignores coefficients of 0.
 
     For example,
         | A B C |                  | 1   B/A C/A |
         | E F G |     results in   | E/F   1 G/F |
-    
+
     This is helpful with obtaining row-echelon form, as
         | A B C D |                | 1   B/A C/A D/A |
         | 0 E F G |   results in   | 0   1   F/E G/E |
@@ -34,6 +35,7 @@ def divide_by_diagonal_coefficient(matrix: FormattedMatrix) -> FormattedMatrix:
     """
     matrix = [[Fraction(i) / row[pos] for i in row] for pos, row in enumerate(matrix) if row[pos]]
     return matrix
+
 
 def reduced_row_echelon(matrix: FormattedMatrix) -> FormattedMatrix:
     """Formats a matrix into Reduced Row Echelon Form"""
@@ -43,7 +45,7 @@ def reduced_row_echelon(matrix: FormattedMatrix) -> FormattedMatrix:
         assert all(len(row) == 4 for row in matrix)
     except AssertionError as e:
         raise ValueError("Matrix must be be of dimensions 3x4") from e
-    
+
     # If 0s occur at the top, it *will* cause issues
     matrix.sort(key=lambda i: -abs(i[0]))
     _print_matrix(matrix)
@@ -63,6 +65,7 @@ def reduced_row_echelon(matrix: FormattedMatrix) -> FormattedMatrix:
     # Ensure first non-zero coefficient is equal to 1
     matrix = divide_by_diagonal_coefficient(matrix)
     _print_matrix(matrix)
+
     # Eliminate remaining non-diagonal zeros
     matrix[0] = _reduce_pos_to_zero(1, matrix[0], matrix[1])
     matrix[0] = _reduce_pos_to_zero(2, matrix[0], matrix[2])
@@ -75,12 +78,19 @@ def reduced_row_echelon(matrix: FormattedMatrix) -> FormattedMatrix:
 
 def display_rre_steps(_input: list[list[num]] | list[list[str]] | str | None = None) -> FormattedMatrix:
     if not _input:
-        _input = input("Matrix?")
+        _input = input("Matrix? ")
     if isinstance(_input, str):
-        _input = [row.split(",") for row in _input.split(";")]
+        _input = [row.split(",") for row in _input.strip().split(";") if row]
     resp: FormattedMatrix = [[Fraction(i) for i in row] for row in _input]
     return reduced_row_echelon(resp)
 
 
 rre = display_rre_steps
 RRE = display_rre_steps
+
+if __name__ == "__main__":
+    # The program can also be called with syntax like:
+    # python rre.py 1,2,-3,-3 -2,3,2,-25 -1,2,-3,-11
+    import sys
+
+    display_rre_steps(";".join(sys.argv[1:]))
